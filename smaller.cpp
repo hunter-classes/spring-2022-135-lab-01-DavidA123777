39326c46b78054206260bf60fe3786d44d6cb532
Super221 <83670808+DavidA123777@users.noreply.github.com>
2022-01-31 14:21:44 +0000

    added smaller.cpp and imported from github

---
+++ smaller.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cout << "Please enter in an integer: " << endl;
+  cin >> a;
+  cout << "Please enter in another integer: " << endl;
+  cin >> b;
+
+  if (a < b){
+    cout << "The smaller of the two is: " << a << endl;
+  }
+  if (b < a){
+    cout << "The smaller of the two is: " << b << endl;
+  }
+
+
+
+
+
+
+  return 0;
+}
